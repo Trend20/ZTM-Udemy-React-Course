6ca556d1bae09aea34b7ee90c37e29ca0d312267
Trend20 <enockomondi305@gmail.com>
2022-11-15 08:32:58 +0300

    adding application routes

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,10 +1,36 @@
 import './categories.styles.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home'
+
+const Navigation = () =>{
+  return(
+    <div>
+      <div>
+        <h1>I am the navigation bar component</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () =>{
+  return(
+    <div>
+      <div>
+        <h1>I am the Shop bar component</h1>
+      </div>
+    </div>
+  )
+}
+
+
 const App = () =>{
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
