f158759e63816fde913d99daf2c0b6f8c35fb7e5
Trend20 <enockomondi305@gmail.com>
2022-11-16 08:24:56 +0300

    adding sign-in and firebase

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -2,6 +2,7 @@ import './categories.styles.scss';
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home'
 import Navigation from './routes/navigation/navigation'
+import SignIn from './routes/sign-in/sign-in';
 
 const Shop = () =>{
   return(
@@ -20,6 +21,7 @@ const App = () =>{
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
