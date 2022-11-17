4b62f1be1b76d7cec0695f5ea6df2855d2031ce5
Trend20 <enockomondi305@gmail.com>
2022-11-17 05:54:49 +0300

    adding form references

--- crown-clothing/src/components/sign-up-form/sign-up-form.jsx
+++ crown-clothing/src/components/sign-up-form/sign-up-form.jsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+
 const SignUp = () =>{
+
+  // initialize state
+  const [formFields, setFormFields ] = useState(defaultFormFields);
+
+  // destructure form fields
+  const { displayName, email, password, confirmPassword } = formFields;
+
+
+  // handle change function
+  const handleChange = (e) =>{
+    
+  }
   return(
     <div className="sign-up">
       <h3>Sign Up with Email and Password</h3>
       <form>
         <label>Display Name</label>
-        <input type="text" required />
+        <input type="text" required name="displayName" value={displayName} />
 
         <label>Email</label>
-        <input type="email" required />
+        <input type="email" required name="email" value={email} />
 
         <label>Password</label>
-        <input type="password" required />
+        <input type="password" required name="password" value={password} />
 
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input type="password" required name="confirmPassword" value={confirmPassword} />
+
+        <button type="submit">Sign Up</button>
       </form>
     </div>
   )
--- crown-clothing/src/routes/sign-in/sign-in.jsx
+++ crown-clothing/src/routes/sign-in/sign-in.jsx
@@ -1,4 +1,5 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import SignUp from "../../components/sign-up-form/sign-up-form";
 
 const SignIn = () =>{
 
@@ -11,6 +12,7 @@ const SignIn = () =>{
     <div>
       <h1>This is the sign in page!</h1>
       <button onClick={logGoogleUser}>Sign in with google</button>
+      <SignUp />
     </div>
   )
 }
